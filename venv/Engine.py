024f1e1461673497583c1c2841615eb86bcc6324
oleh <kuzanoleg@gmail.com>
2020-06-02 16:00:33 +0300

    Initial

---
+++ venv/Engine.py
@@ -0,0 +1,41 @@
+import itertools
+import pygame as pg
+import pygame_gui as gui
+
+pg.init()
+
+BLACK = pg.Color('black')
+WHITE = pg.Color('white')
+
+icon = pg.image.load('../data/icons/virus2.png')
+pg.display.set_icon(icon)
+pg.display.set_caption("Simulcarum")
+
+screen = pg.display.set_mode((360, 360))
+clock = pg.time.Clock()
+
+colors = itertools.cycle((WHITE, BLACK))
+tile_size = 20
+width, height = 8*tile_size, 8*tile_size
+background = pg.Surface((width, height))
+
+for y in range(0, height, tile_size):
+    for x in range(0, width, tile_size):
+        rect = (x, y, tile_size, tile_size)
+        pg.draw.rect(background, next(colors), rect)
+    next(colors)
+
+game_exit = False
+while not game_exit:
+    for event in pg.event.get():
+        if event.type == pg.QUIT:
+            game_exit = True
+
+    screen.fill((60, 70, 90))
+    screen.blit(background, (100, 100))
+
+    pg.display.flip()
+    clock.tick(30)
+
+pg.quit()
+quit()
